cd4db6643c6a05f9e1179308d5db828f894a8ce3
jersongerman <jersontaipemiranda@gmail.com>
2026-07-13 03:28:13 -0500

    feat: expansion de imagen en python

---
+++ matematica_computacional/procesamiento_de_imagenes/expansion.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.withdraw()
+
+
+
+
+print("Expansion de una Imagen")
+
+image_path = input("Ingresa el nombre de la imagen con su extensión PNG o JPG: ")
+
+# validar imagen con extension png o jpg
+
+
+
+print(f"procesaremos la imagen: {image_path}...")
